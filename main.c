e858f5cc7d7f6d4346c692f716b279eef3106706
Miguel <miguelcunha1205@gmail.com>
2024-09-18 19:47:27 -0300

    att

--- main.c
+++ main.c
@@ -1,27 +1,30 @@
 #include <stdio.h>
 
 int main() {
+    // Matriz de preços
     float precos[3][2] = {
         {15.00, 12.50},
         {13.00, 7.50},
         {100.00, 97.00}
     };
 
-    float medias[3];
-    float *ponteiroPreco;
-    int n = 2;
+    float medias[3]; // Vetor para armazenar as médias
+    float *ponteiroPreco; // Ponteiro para os preços
+    int n = 2; // Número de preços por material
 
+    // Cálculo das médias
     for (int i = 0; i < 3; i++) {
-        float soma = 0;
-        ponteiroPreco = precos[i];
+        float soma = 0; // Inicializa a soma como 0
+        ponteiroPreco = precos[i]; // Ponteiro aponta para a linha atual da matriz
 
         for (int j = 0; j < n; j++) {
-            soma += *(ponteiroPreco + j);
+            soma += *(ponteiroPreco + j); // Soma os preços usando o ponteiro
         }
 
-        medias[i] = soma / n;
+        medias[i] = soma / n; // Calcula a média e armazena no vetor de médias
     }
 
+    // Exibe as médias
     printf("Média de preço por material:\n");
     for (int i = 0; i < 3; i++) {
         printf("%.2f\t", medias[i]);
